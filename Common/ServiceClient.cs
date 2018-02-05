ee73418c90367e7d04d44db31c5668cf690cb6bb
Brian Lee <brian.lee@ipreo.com>
2018-02-05 11:41:10 -0600

    HttpClient static updates

--- Common/ServiceClient.cs
+++ Common/ServiceClient.cs
@@ -6,15 +6,22 @@ namespace Common
 {
     public class ServiceClient<T>
     {
+        private readonly static HttpClient _client = new HttpClient();
+        private const string _mediaType_JSON = "application/json";
+
+        private static HttpClient GetHttpClient()
+        {
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_mediaType_JSON));
+            return _client;
+        }
+
         public Result<T> GetData(string url)
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("User-Agent", "Test");
+            var client = GetHttpClient();
 
             var responseJSON = client.GetStringAsync(url).Result;
+
             var data = JsonConvert.DeserializeObject<Result<T>>(responseJSON);
 
             return data;
@@ -22,16 +29,12 @@ namespace Common
 
         public Result<T> SearchData(string url, Search search)
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("User-Agent", "Test");
+            var client = GetHttpClient();
 
             var searchJson = JsonConvert.SerializeObject(search);
             var buffer = System.Text.Encoding.UTF8.GetBytes(searchJson);
             var byteContent = new ByteArrayContent(buffer);
-            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue(_mediaType_JSON);
 
             var response = client.PostAsync(url, byteContent).Result;
             var responseJSON = response.Content.ReadAsStringAsync().Result;
